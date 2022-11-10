1cf58d218a8d9eab2bf7879f323e950fdb426a0a
Alejandro León <101680446+AlejandroLeonLzn@users.noreply.github.com>
2022-11-10 15:23:02 -0600

    Update ia.py

--- ia.py
+++ ia.py
@@ -2,7 +2,7 @@ from array import array
 from random import random
 import numpy as np
 #....Sección de funciones.......................................
-#____Función que captura el numero de individuos de la población
+#....Función que captura el numero de individuos de la población
 def cap_num_ind():
     t=int(input("Ingrese tamaño de la población: "))
     if (t%2!=0):
@@ -15,9 +15,9 @@ def calc_num_bits(i,j,p):
     t=round(t)
     t=int(t)
     return(t)
-#....función que contruye una matriz (tam_pob x num_bits)
+#....función que contruye una matriz (tam_pob x num_bits)........
 def crea_matriz(tp,nb):
-    t=np.zeros((tp,nb+1))
+    t=np.zeros((2*tp,nb+1))
     return t
 #....Inicializa la población que va a evolucionar
 def inicia_pob(p,r,c):
@@ -54,6 +54,22 @@ def op_ordenamiento(pob,tp,nb):
                     k=k+1
             j=j+1
         i=i+1
+#....Operador de seleccion para intercambiar genes entre dos padres
+def op_seleccion(pob,tp,nb):
+    i=0
+    pc=int(nb/2)
+    while(i<tp):
+        j=i+1
+        k=0
+        while(k<pc):
+            pob[i+tp][k]=pob[i][k]
+            pob[i+tp+1][k]=pob[j][k]
+            k=k+1
+        while(k<nb):
+            pob[i+tp][k]=pob[j][k]
+            pob[i+tp+1][k]=pob[i][k]
+            k=k+1
+        i=i+2
 #....Sección principal...........................................
 num_generaciones=1
 tam_pobla=cap_num_ind()
@@ -68,6 +84,13 @@ inicia_pob(poblacion,tam_pobla,num_bits)
 while (num_generaciones<=max_generaciones):
     op_evaluacion(poblacion,a,b,presicion,tam_pobla,num_bits)
     op_ordenamiento(poblacion,tam_pobla,num_bits)
+    op_seleccion(poblacion,tam_pobla,num_bits)
     num_generaciones=num_generaciones+1
 
 print (poblacion)
+
+#instalar matplot
+#Tarea construir el algoritmo que de manera aleatoria selecciones o elija en cada generacion
+#una de las 3 formas de formar pareja
+#Aleatorio, elitista y vasconselos
+
